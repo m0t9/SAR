2b7232e559a0c21965de4394141525d6a1aeb8f7
m0t9_ <mateykorinenko@gmail.com>
2020-11-24 18:30:00 +0500

    Added Google Maps, Цифровое благополучие
    Signed-off-by: m0t9_ <mateykorinenko@gmail.com>

--- packets.py
+++ packets.py
@@ -1,3 +1,5 @@
 packet_name = {
-    'Google Music': 'com.google.android.music'
+    'Google Music': 'com.google.android.music',
+    'Google Maps': 'com.google.android.apps.maps',
+    'Цифровое благополучие': 'com.google.android.apps.wellbeing'
 }

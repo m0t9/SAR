67f629500b9690eb541afabd02bc903326047846
m0t9_ <mateykorinenko@gmail.com>
2020-11-25 17:17:31 +0500

    Refactored database taking
    Signed-off-by: m0t9_ <mateykorinenko@gmail.com>

--- cmd.py
+++ cmd.py
@@ -1,11 +1,17 @@
 from os import system, chdir
-from packets import packet_name
+from db_funcs import DatabaseTaker
 
 
 def run(command):
     return system(command)
 
 
-def remove_app(app):
-    chdir('adb')
-    return run('adb shell pm uninstall -k --user 0 ' + packet_name[app])
+def remove_app(app_name, phone_model):
+    dbt = DatabaseTaker()
+    app_packet = dbt.get_packet_app(app_name, phone_model)
+    if app_packet:
+        chdir('adb')
+        return run('adb shell pm uninstall -k --user 0 ' + app_packet)
+    else:
+        return 2
+
--- packets.py
+++
@@ -1,9 +0,0 @@
-packet_name = {
-    'Google Music': 'com.google.android.music',
-    'Google Maps': 'com.google.android.apps.maps',
-    'Yandex Maps': 'ru.yandex.yandexmaps',
-    'Цифровое благополучие': 'com.google.android.apps.wellbeing',
-    'Xiaomi Cloud': 'com.miui.cloudservice',
-    'ShareMe': 'com.xiaomi.midrop',
-    'Mi Credit': 'com.xiaomi.payment'
-}

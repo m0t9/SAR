ca714741aa390bd10a71b669527254fc2717bfb5
m0t9_ <mateykorinenko@gmail.com>
2020-11-28 18:15:44 +0500

    Small bugs fixed
    Signed-off-by: m0t9_ <mateykorinenko@gmail.com>

--- cmd.py
+++ cmd.py
@@ -1,5 +1,5 @@
 from os import system, chdir
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from db_funcs import DatabaseTaker
 
 
@@ -9,24 +9,24 @@ class CommandLine:
         chdir('adb')
 
     # RUN CMD
-    @staticmethod
-    def run(command):
+    def run(self, command):
         return system(command)
 
     # REMOVE SELECTED APP
-    @staticmethod
-    def remove_app(app_name, phone_model, dbt):
+    def remove_app(self, app_name, phone_model, dbt):
+        is_connected = self.connection_check()
+        if not is_connected:
+            return 2
         try:
             app_packet = dbt.get_packet_app(app_name, phone_model)
             if app_packet:
-                return run('adb shell pm uninstall -k --user 0 ' + app_packet)
+                return self.run('adb shell pm uninstall -k --user 0 ' + app_packet)
             else:
                 return 2
-        except:
-            return 3
+        except CalledProcessError:
+            return 2
 
     # DEVICE CONNECTION CHECK
-    @staticmethod
     def connection_check(self):
         output = (check_output('adb devices')).decode('utf-8')
         output = len(output.split('\r\n')) - 3
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic, QtGui, QtCore, QtWidgets, Qt
-from cmd import remove_app, run
+from cmd import CommandLine
 from db_funcs import DatabaseTaker
-from errors import decode_error
+from errors import decode_error, make_verdict
 
 
 class App(QMainWindow):
@@ -13,6 +13,8 @@ class App(QMainWindow):
         uic.loadUi('ui.ui', self)
 
         self.dbt = DatabaseTaker()
+        self.cmd = CommandLine()
+
         self.model.addItems(self.dbt.models_list)
         self.current_phone_model = self.dbt.models_list[0]
 
@@ -25,17 +27,22 @@ class App(QMainWindow):
         self.apps.activated.connect(self.add_delete_app)
         self.remove_apps_button.clicked.connect(self.remove_choosed_apps)
 
+
     def load_compatible_apps(self, index):
+        self.progress_bar.setValue(0)
         self.remove.clear()
         self.progress_log.clear()
         self.compatible_apps.clear()
         self.apps.clear()
+
         phone_model = self.dbt.models_list[index]
         self.current_phone_model = phone_model
+
         self.compatible_apps = self.dbt.get_phone_apps(phone_model)
         self.apps.addItems(self.compatible_apps)
 
     def add_delete_app(self, index):
+        self.progress_bar.setValue(0)
         self.progress_log.clear()
         application = self.compatible_apps[index]
         if application in self.remove:
@@ -47,10 +54,22 @@ class App(QMainWindow):
     def remove_choosed_apps(self):
         self.progress_log.clear()
         result = list()
-        for item in self.remove:
-            verdict = item + ': ' + decode_error(remove_app(item, self.current_phone_model, self.dbt))
-            result.append(verdict)
-        self.progress_log.appendPlainText('\n\n'.join(result))
+
+        full = len(self.remove)
+        current = 0
+
+        if full:
+            self.progress_bar.setValue(current // full * 100)
+            for item in self.remove:
+                verdict = make_verdict(item,
+                                       self.cmd.remove_app(item, self.current_phone_model, self.dbt))
+                result.append(verdict)
+                current += 1
+                self.progress_bar.setValue(current // full * 100)
+
+            self.progress_log.appendPlainText('\n\n'.join(result))
+            self.remove.clear()
+            result.clear()
 
 
 if __name__ == '__main__':

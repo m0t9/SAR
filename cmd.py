26e1f8ce2afe18c784a920d9c29fbc41f27c4284
m0t9_ <mateykorinenko@gmail.com>
2021-01-07 15:25:52 +0500

    Added multiplatform for Linux and Windows OS.
    Signed-off-by: m0t9_ <mateykorinenko@gmail.com>

--- cmd.py
+++ cmd.py
@@ -1,16 +1,13 @@
-from os import system, chdir
-from subprocess import check_output, CalledProcessError
+from os import chdir
+from subprocess import check_output, CalledProcessError, call
+from sys import platform
 
 
 class CommandLine:
     # SET ON
     def __init__(self):
-        chdir('adb')
-        system('adb start-server')
-
-    # RUN CMD
-    def run(self, command):
-        return system(command)
+        chdir('adb')  # CHANGING DIRECTORY FOR WINDOWS
+        call(['adb', 'start-server'])
 
     # REMOVE SELECTED APP
     def remove_app(self, app_name, phone_model, dbt):
@@ -20,7 +17,7 @@ class CommandLine:
         try:
             app_packet = dbt.get_packet_app(app_name, phone_model)
             if app_packet:
-                return self.run('adb shell pm uninstall -k --user 0 ' + app_packet)
+                return call(['adb', 'shell', 'pm', 'uninstall', '-k', '--user 0', app_packet])
             else:
                 return 2
         except CalledProcessError:
@@ -28,8 +25,12 @@ class CommandLine:
 
     # DEVICE CONNECTION CHECK
     def connection_check(self):
-        output = (check_output('adb devices')).decode('utf-8')
-        output = len(output.split('\r\n')) - 3
+        output = (check_output(['adb', 'devices'])).decode('utf-8')
+
+        if platform == 'linux' or platform == 'linux2':
+            output = len(output.split('\n')) - 3
+        elif platform == 'win32':
+            output = len(output.split('\r\n')) - 3
 
         if output:
             return True
